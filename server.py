b638ffac1b3e6daf378dcb62e4a7765f4d55225c
Bryan Schlief <bschlief@gmail.com>
2014-10-30 01:34:27 -0700

    Parse out opcode

--- server.py
+++ server.py
@@ -1,16 +1,35 @@
 import SocketServer
+import struct
+import logging
 
-class UDPHandler(SocketServer.BaseRequestHandler):
-    """
-    Handle requests
-    """
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+logger = logging.getLogger("tftp-client")
+logger.addHandler(console)
+
+OP_RRQ = 1
+OP_WRQ = 2
+OP_ACK = 3
+OP_DATA = 4
+OP_ERROR = 5
+
+class UDPHandler(SocketServer.DatagramRequestHandler):
+
+    def get_opcode(self, data):
+        opcode = struct.unpack("!H", data[0:2])
+        return opcode[0]
 
     def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        print data
+        data, socket = self.request
+        opcode = self.get_opcode(data)
+
+        if opcode == OP_RRQ:
+            print("Received a read request")
+        elif opcode == OP_ACK:
+            print("received an ack")
+
         socket.sendto(data.upper(), self.client_address)
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
