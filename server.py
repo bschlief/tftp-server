d148af33617ce0421e6bd63fb23c31d610a10412
Bryan Schlief <bschlief@gmail.com>
2014-10-30 01:49:33 -0700

    Filenames are parsing out.
    Need to finish/rework logger configuration (perhaps with basicConfig())
    Need to open a file, partition it, and send it in chunks back to client.

--- server.py
+++ server.py
@@ -15,16 +15,21 @@ OP_ERROR = 5
 
 class UDPHandler(SocketServer.DatagramRequestHandler):
 
-    def get_opcode(self, data):
-        opcode = struct.unpack("!H", data[0:2])
+    def get_opcode(self, packed_opcode):
+        opcode = struct.unpack("!H", packed_opcode)
         return opcode[0]
+
+    def get_filename_and_mode(self, data):
+        filename, mode, _ = data.split("\0")
+        return (filename, mode)
 
     def handle(self):
         data, socket = self.request
-        opcode = self.get_opcode(data)
+        opcode = self.get_opcode(data[0:2])
 
         if opcode == OP_RRQ:
-            print("Received a read request")
+            filename, mode = self.get_filename_and_mode(data[2:])
+
         elif opcode == OP_ACK:
             print("received an ack")
 
